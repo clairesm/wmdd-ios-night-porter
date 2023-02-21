85c4c9f7cf43118ec613332889dc5df2bb05e78f
Claire Simbulan <elleclairesimbulan@gmail.com>
2023-02-20 20:49:02 -0800

    first commit

---
+++ Night Porter/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Night Porter
+//
+//  Created by Elhynnie Claire Simbulan on 2023-02-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+        
+        //get everything contained in the top level view
+        let everything = view.subviews
+        for eachView in everything {
+            //is it a label?
+            if eachView is UILabel {
+                //downcast as UILabel
+                let currentLabel = eachView as! UILabel
+                currentLabel.textColor = UIColor.lightGray
+            }
+            eachView.backgroundColor = UIColor.green
+        }
+    }
+    
+}
+
